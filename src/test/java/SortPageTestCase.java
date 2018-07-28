c9c387d7f5370795b3942c0339772d31b5df050e
roma7481 <roma7481@gmail.com>
2018-07-28 09:30:36 -0400

    Firefox test added for apples

--- src/test/java/SortPageTestCase.java
+++ src/test/java/SortPageTestCase.java
@@ -1,11 +1,24 @@
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SortPageTestCase {
 
     @Test
-    public void applesSortingOrderTest(){
+    public void applesSortingOrderTestChrome(){
         SortPage sortPage = new SortPage();
-        Assert.assertTrue(sortPage.appleSorting());
+        WebDriver driver = new ChromeDriver();
+        System.out.println("" + sortPage.appleSorting(driver));
+        Assert.assertTrue(sortPage.appleSorting(driver));
+    }
+
+    @Test
+    public void applesSortingOrderTestFireFox(){
+        SortPage sortPage = new SortPage();
+        WebDriver driver = new FirefoxDriver();
+        Assert.assertTrue(sortPage.appleSorting(driver));
     }
 }
