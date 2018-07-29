25e6e3a14babe6f3ddf8a480e6faa3aaf3f84b36
roma7481 <roma7481@gmail.com>
2018-07-28 21:23:25 -0400

    French added

--- src/main/java/SortPage.java
+++ src/main/java/SortPage.java
@@ -12,39 +12,35 @@ import java.util.concurrent.TimeUnit;
 
 public class SortPage {
 
-    public boolean appleSorting(){
-
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    private WebDriver driver;
+    private ManageDriver manageDriver;
+    public SortPage(WebDriver driver){
+        this.driver = driver;
+        manageDriver = new ManageDriver(this.driver);
+    }
+    public boolean appleSorting(String keyWordToSearch,String language){
+        String url = "https://www.loblaws.ca";
+        manageDriver.openWebSiteMaximizeWindow(url);
         WebDriverWait wait = new WebDriverWait(driver, 30000);
-        driver.get("https://www.loblaws.ca");
-        driver.manage().window().maximize();
-        WebElement element = driver.findElement(By.id("search-bar"));
-        element.sendKeys("apples");
-        element.submit();
+        if(language.equals("FR")){
+            driver.findElement(By.linkText("FR")).click();
+        }
+        manageDriver.searchKeyWord(keyWordToSearch);
         driver.findElement(By.xpath("//div[3]/div/ul/li[3]/button")).click();
-//        try {
-//            Thread.sleep(3000);
-//        } catch (InterruptedException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-        wait.until(d -> (boolean)((JavascriptExecutor)driver).executeScript("return jQuery.active == 0"));
+        manageDriver.waitJqueryDone();
         while(driver.findElement(By.cssSelector("button.btn-inline-link.btn-show-more")).isDisplayed()){
             ((JavascriptExecutor) driver)
                     .executeScript("window.scrollTo(0, document.body.scrollHeight)");
                 driver.findElement(By.cssSelector("button.btn-inline-link.btn-show-more")).click();
-                wait.until(d -> (boolean)((JavascriptExecutor)driver).executeScript("return jQuery.active == 0"));
-
+                manageDriver.waitJqueryDone();
         }
         List<WebElement> list = driver.findElements(By.className("reg-price-text"));
         List<Double> prices = new ArrayList<>();
         list.forEach(e->prices.add(getPrice(e)));
-        boolean sorted = Ordering.natural().isOrdered(prices);
-        driver.quit();
+        boolean sorted = Ordering.natural().reverse().isOrdered(prices);
         return sorted;
     }
     private Double getPrice(WebElement e) {
-        return Double.valueOf( e.getText().replace("$",""));
+        return Double.valueOf( e.getText().replace("$","").replace(",","."));
     }
 }
