9ae1575a229bd6012fe5e341a605e621f59f725c
roma7481 <roma7481@gmail.com>
2018-07-29 22:33:37 -0400

    small screen added. firefox test cases ignored

--- src/test/java/SuperStoreTestCases.java
+++ src/test/java/SuperStoreTestCases.java
@@ -1,11 +1,15 @@
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SuperStoreTestCases {
 
     private static final String ORANGES = "oranges";
+    private static final int width = 600;
+    private static final int height  = 600;
 
     @Test
     public void applesSortingOrderTestChrome(){
@@ -13,6 +17,26 @@ public class SuperStoreTestCases {
         SuperStorePage superStorePage = new SuperStorePage(driver);
         Assert.assertTrue(superStorePage.verifyDealBadge(ORANGES));
         driver.quit();
-
+    }
+    @Test
+    public void applesSortingOrderTestChromeSmallScreen(){
+        WebDriver driver = new ChromeDriver();
+        SuperStorePage superStorePage = new SuperStorePage(driver);
+        Assert.assertTrue(superStorePage.verifyDealBadgeSmallScreen(ORANGES,width,height));
+        driver.quit();
+    }
+    @Ignore
+    public void applesSortingOrderTestFireFox(){
+        WebDriver driver = new FirefoxDriver();
+        SuperStorePage superStorePage = new SuperStorePage(driver);
+        Assert.assertTrue(superStorePage.verifyDealBadge(ORANGES));
+        driver.quit();
+    }
+    @Ignore
+    public void applesSortingOrderTestFireFoxSmallScreen(){
+        WebDriver driver = new FirefoxDriver();
+        SuperStorePage superStorePage = new SuperStorePage(driver);
+        Assert.assertTrue(superStorePage.verifyDealBadgeSmallScreen(ORANGES,width,height));
+        driver.quit();
     }
 }
